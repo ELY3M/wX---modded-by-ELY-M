9f3c4c63a6ef48b2cdaed9d56a5a03602d13ab4c
ELY M <>
2024-07-31 22:25:56 -0500

    55903-elys

--- gitpush.bat
+++ gitpush.bat
@@ -1,7 +1,6 @@
 grep -Po 'android:versionName="(.*)"' app/src/main/AndroidManifest.xml | sed -r 's/.*android:versionName="([^ ]+)".*/\1/'>versionname
 set /p versionname=<versionname
 del versionname
-del set
 git add .  
 git commit -m %versionname%
 git push 
